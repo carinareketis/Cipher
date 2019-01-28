b1ddb96bcac9914b0cdbcb44f1a76a9b4d8d363f
carinareketis <carina.reketis@gmail.com>
2019-01-28 14:11:03 -0200

    Atualização de html, css e javascript

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,50 @@
-window.cipher = {
-  // ... 
-};
+var message = prompt("Digite sua mensagem:");
+
+var offset = prompt("Digite seu deslocamento:");
+
+document.getElementById('message').innerHTML = (message);
+
+
+function stringOffset(string, offset){
+  let asciiCode = string.charCodeAt(0);
+
+  if(asciiCode >= 65 && asciiCode <= 90){
+    asciiCode = ((asciiCode - 65 + offset) % 26);
+    if (asciiCode < 0) asciiCode = 91 + asciiCode;    
+  }
+
+  if(asciiCode >= 97 && asciiCode <= 122){
+    asciiCode = ((asciiCode - 97 + offset) % 26);
+    if (asciiCode < 0) asciiCode = 123 + asciiCode;
+    else asciiCode += 97;
+  }
+  return String.fromCharCode(asciiCode);   
+}
+
+
+function cipherEncode(string, offset){
+  
+  let textCoded = "";
+
+  for (let i=0; i<string.length; i++){
+    textCoded += stringOffset(string[i], offset);  
+  }
+  return textCoded;  
+}
+
+var coded = cipherEncode(message.toLowerCase(), parseInt(offset));
+
+document.getElementById('message-cipher').innerHTML = (coded);
+
+
+function cipherDecode(string, offset){
+  return cipherEncode(string, -offset);
+}
+
+document.getElementById('message-decipher').innerHTML = (cipherDecode(coded, offset));
+
+
+
+
+
+
